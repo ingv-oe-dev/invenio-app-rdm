dc1ef5ee7ca1b08aa9baaace03b8074b58175431
mtorrisi <mario.torrisi@ingv.it>
2022-08-22 14:50:45 +0200

    Some few code reviewer

--- invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/components/LocationsMapPreview.js
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/components/LocationsMapPreview.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
-import { CustomMarker } from "./CustomMarker";
+import { CustomLoader, CustomMarker } from "./CustomComponents";
 import PropTypes from "prop-types";
 
 export const LocationsMapPreview = ({ markers }) => {
+  const [loading, setLoading] = useState(true);
+  const [tileLayer, setMapContainer] = useState({});
   const center = markers[0].latlng;
-  return (
-    <MapContainer center={center} zoom={10} scrollWheelZoom markers={markers}>
+
+  useEffect(() => {
+    setMapContainer(
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        onload={setLoading(false)}
       />
+    );
+  }, []);
+
+  return loading ? (
+    <CustomLoader />
+  ) : (
+    <MapContainer center={center} zoom={10} scrollWheelZoom markers={markers}>
+      {tileLayer}
       {markers.map((position) => (
         <CustomMarker key={position.latlng.toString()} position={position} />
       ))}
